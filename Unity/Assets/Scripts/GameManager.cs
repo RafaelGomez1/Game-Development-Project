f5445c35694d38228d6dc17bf419cf1475b0858d
Diego <diegovd0296@gmail.com>
2017-07-21 22:40:30 +0200

    Lighting in Subconscious done

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Parecido una clase pero solo puede tomar esos 3 valores.
+public enum GameState
+{
+    menu,
+    inTheGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+    public GameState currentGameState = GameState.menu;
+
+    //Patron de configuracion Singleton, permite una nuica instanciacion para ser usada multiples veces por distintos Gameobjectes.
+    public static GameManager sharedInstance;
+
+    //Antes de cargar la escena, necesitamos que la variable sharedInstance, significa la misma clase.
+    private void Awake()
+    {
+       
+        sharedInstance = this;
+    }
+
+   
+    void Start()
+    {
+        currentGameState = GameState.menu;
+        StartGame();
+    }
+
+    void Update()
+    {   
+        /*
+        if (Input.GetButtonDown("r"))
+        {
+            if (currentGameState != GameState.inTheGame)
+            {
+                StartGame();
+            }
+        }
+        */
+    }
+
+    public void StartGame()
+    {
+        PlayerControl.sharedInstance.StartGame();
+        ChangeGameState(GameState.inTheGame);
+    }
+
+   
+    public void GameOver()
+    {
+        ChangeGameState(GameState.gameOver);
+    }
+
+   
+    public void BackToMainMenu()
+    {
+        ChangeGameState(GameState.menu);
+    }
+
+    private void ChangeGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            //Muestra el menu principal
+        }
+        else if (newGameState == GameState.inTheGame)
+        {
+            //La escena se configura para jugar.
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            //Pantalla de fin de juego
+        }
+
+        currentGameState = newGameState;
+    }
+
+
+
+
+
+}
---
+++ Unity/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    //Singleton para trigger y mas (Solo queremos un unico personage
+    //por pantalla.
+    public static PlayerControl sharedInstance;
+
+    //Initial position of player
+    private Vector3 startPosition;
+
+
+    private Rigidbody2D rigidBody;
+    public float jumpForce = 6.0f;
+    public Animator animator;
+    public float runningSpeed = 6.0f;
+    public LayerMask groundLayerMask;
+    private bool facingRight = true;
+
+    void Awake()
+    {
+        sharedInstance = this;
+        //recuperar variable del jugador para aplicar fisicas:
+        startPosition = this.transform.position;
+        rigidBody = GetComponent<Rigidbody2D>();
+        facingRight = true;
+
+        animator.SetBool("IsMove", false);
+		animator.SetBool ("IsJump", false);
+        
+
+    }
+
+    // Use this for initialization
+    public void StartGame()
+    {
+        this.transform.position = startPosition;
+        rigidBody.velocity = new Vector2(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (GameManager.sharedInstance.currentGameState == GameState.inTheGame)
+        {
+            if (IsOnTheFloor())
+            {
+                animator.SetBool("IsJump", false);
+                Jump();
+            }
+            Move();
+            Flip(horizontal);
+        }
+		
+        
+
+    }
+
+    void Jump()
+    {
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rigidBody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+
+    }
+
+    void Move()
+    {
+        
+        if (Input.GetKey (KeyCode.D)) {
+
+
+            rigidBody.velocity = new Vector2 (runningSpeed, rigidBody.velocity.y);
+			animator.SetBool("IsMove", true);
+
+		} else if (Input.GetKey (KeyCode.A)) {
+            
+            
+            rigidBody.velocity = new Vector2 (-runningSpeed, rigidBody.velocity.y);
+			animator.SetBool("IsMove", true);
+
+		} else {
+			animator.SetBool("IsMove", false);
+		}
+        
+
+
+    }
+
+	bool IsOnTheFloor(){
+		if (Physics2D.Raycast (rigidBody.transform.position, Vector2.down, 1.3f, groundLayerMask.value)) {
+			animator.SetBool ("IsJump", false);
+			return true;
+		} else {
+			animator.SetBool ("IsJump", true);
+			return false;
+		}
+	}
+
+    void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    public void KillPlayer()
+    {
+        Debug.Log("Muerto");
+        //GameManager.sharedInstance.GameOver();
+        StartGame();
+        
+    }
+}
+
+
