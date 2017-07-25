fd3bfd9a89fae7c49100db40f32ce067962178cb
Halapan <erictoper@gmail.com>
2017-07-25 11:36:29 +0200

    Reload

---
+++ Unity/Assets/Scripts/UI Scripts/LifeAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeAnimation : MonoBehaviour {
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator.SetBool("IsLifeHud", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetBool("IsLifeHud", true);
+    }
+}
